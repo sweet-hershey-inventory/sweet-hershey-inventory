7e300bd1c93e1c23c3f5196e21e24504abbf26ee
sweet-hershey-inventory <sweethersheyloyverse@gmail.com>
2026-02-21 13:20:16 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,6 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyAyjwL_zA6VT6GMO2XUxXKNhYpeP6LHI0U",
   authDomain: "sweethersheytrackinventory.firebaseapp.com",
   projectId: "sweethersheytrackinventory",
+  storageBucket: "sweethersheytrackinventory.firebasestorage.app",
+  messagingSenderId: "1070869731224",
+  appId: "1:1070869731224:web:cda15c136ab95cc6c5bded"
 };
 
 const app = initializeApp(firebaseConfig);
